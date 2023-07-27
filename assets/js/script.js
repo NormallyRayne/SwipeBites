bfda2b0fa6272297ce7df5b3d4996a570c4d2746
unknown <rtmc1019@gmail.com>
2023-07-27 19:41:18 -0400

    added the two API'S

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,26 +1,54 @@
-const url = 'https://worldwide-restaurants.p.rapidapi.com/search';
-const options = {
-    method: 'POST',
-    headers: {
+// First API
+async function restaurantsApi() {
+    const url = 'https://worldwide-restaurants.p.rapidapi.com/reviews';
+    const options = {
+      method: 'POST',
+      headers: {
         'content-type': 'application/x-www-form-urlencoded',
-        'X-RapidAPI-Key': 'da46b2c39amshc1d73aed7781c16p145075jsn396e86173253',
-        'X-RapidAPI-Host': 'worldwide-restaurants.p.rapidapi.com'
-    },
-    body: new URLSearchParams({
+        'X-RapidAPI-Key': '61185c9298msh05e9087888395dap1a2be8jsnc2195cf9db75',
+        'X-RapidAPI-Host': 'worldwide-restaurants.p.rapidapi.com',
+      },
+      body: new URLSearchParams({
+        currency: 'USD',
+        limit: '15',
         language: 'en_US',
-        limit: '30',
-        location_id: '297704',
-        currency: 'USD'
-    })
-};
-async function gitapi() {
+        location_id: '15333482',
+      }),
+    };
+  
     try {
-        const response = await fetch(url, options);
-        const result = await response.json();
-        console.log(result);
+      const response = await fetch(url, options);
+      console.log(response);
+      const result = await response.json();
+      console.log(result);
     } catch (error) {
-        console.error(error);
+      console.error(error);
     }
-
-} 
-gitapi()
+  }
+  
+  // Call the asynchronous function
+  restaurantsApi();
+  
+  // Second API
+  async function secondApi() {
+    const url = 'https://ip-geo-location.p.rapidapi.com/ip/check?format=json';
+    const options = {
+      method: 'GET',
+      headers: {
+        'X-RapidAPI-Key': '61185c9298msh05e9087888395dap1a2be8jsnc2195cf9db75',
+        'X-RapidAPI-Host': 'ip-geo-location.p.rapidapi.com',
+      },
+    };
+  
+    try {
+      const response = await fetch(url, options);
+      const result = await response.json();
+      console.log(result);
+    } catch (error) {
+      console.error(error);
+    }
+  }
+  
+  // Call the second API function
+  secondApi();
+  
